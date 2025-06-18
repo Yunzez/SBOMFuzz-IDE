3eab80111f76256be41d7c990753d96954ea7647
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2025-06-18 17:32:08 -0400

    update core

--- sbomfuzz/toy-rust/src/main.rs
+++ sbomfuzz/toy-rust/src/main.rs
@@ -19,15 +19,12 @@ fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
-<<<<<<< Updated upstream
-=======
 fn add_random(a: i32) -> i32 {
     let mut rng = rand::thread_rng();
     let random_number: i32 = rng.gen_range(1..=10);
     a + random_number
 }
 
->>>>>>> Stashed changes
 fn subtract(a: i32, b: i32) -> i32 {
     a - b
 }
