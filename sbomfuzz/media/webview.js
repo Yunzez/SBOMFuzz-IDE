3577b5e0e498d750208fc74bee8f911b54a639bd
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2025-06-19 10:55:53 -0400

    update

--- sbomfuzz/media/webview.js
+++ sbomfuzz/media/webview.js
@@ -26,8 +26,7 @@ window.addEventListener("message", (event) => {
   }
 });
 
-// Example entry list injection
-const entries = ["mod::func1", "mod::func2"];
+
 const targetContainer = document.getElementById("entry-list");
 const entryButton = document.getElementById("entry-button");
 entryButton.addEventListener("click", () => {
@@ -35,6 +34,7 @@ entryButton.addEventListener("click", () => {
 });
 
 const startButton = document.getElementById("start-analyzer");
+
 startButton.addEventListener("click", () => {
   vscode.postMessage({
     command: "runAnalyzer",
@@ -42,12 +42,7 @@ startButton.addEventListener("click", () => {
     projectPath: pathSelected,
   });
 });
-entries.forEach((name) => {
-  const targetDiv = document.createElement("div");
-  targetDiv.className = "item";
-  targetDiv.innerHTML = `${name} <button onclick="runTarget('${name}')">Run</button>`;
-  targetContainer.appendChild(targetDiv);
-});
+
 
 window.addEventListener("message", (event) => {
   const message = event.data;
@@ -59,15 +54,10 @@ window.addEventListener("message", (event) => {
     for (let i = 0; i < results.length; i++) {
       const result = results[i];
       const resultDiv = document.createElement("div");
-      resultDiv.className = "result-item";
-      resultDiv.style.cursor = "pointer";
-      resultDiv.style.border = "1px solid #ccc";
-      resultDiv.style.borderRadius = "6px";
-      resultDiv.style.margin = "6px 0";
-      resultDiv.style.padding = "8px";
+      resultDiv.className = "result-item function-button";
       resultDiv.innerHTML = `
       <div style="font-weight:bold; margin-bottom:4px;">${result.functionName}</div>
-      <div style="color:#555;">${result.functionLocation.filePath}</div>
+      <div>${result.functionLocation.filePath}</div>
     `;
 
       resultDiv.onclick = () => {
