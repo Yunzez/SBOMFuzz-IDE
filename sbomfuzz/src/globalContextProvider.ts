8bc6ef02fcafc805ffe95c6fc875be713cfadfb6
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2025-07-01 19:59:59 -0400

    bug fix

--- sbomfuzz/src/globalContextProvider.ts
+++ sbomfuzz/src/globalContextProvider.ts
@@ -1,24 +1,24 @@
 /**
  * @fileoverview
  * Provides a global context provider for the SBOMFuzz-IDE VSCode extension.
- * 
+ *
  * This module manages shared state across the extension, including the fuzz root,
  * project root, and analysis results. It exposes functions to initialize and access
  * this global context, and integrates with project analysis utilities.
- * 
+ *
  * @module globalContextProvider
  */
 import path from "path";
+import fs from "fs";
 import * as vscode from "vscode";
 import { loadFunctionResults } from "./functionOutputProcesser";
 import { findCargoProjectRoot, findFuzzRoot } from "./util";
 import { runRustAnalyzer } from "./rustAnalyzerStart";
 
-
 export interface SharedContext {
   fuzzRoot?: string;
   projectRoot?: string;
-  extensionPath?: string; 
+  extensionPath?: string;
   results?: any[];
 }
 
@@ -34,8 +34,8 @@ export async function useGlobalContext(context: vscode.ExtensionContext) {
   const projectPath = context.extensionPath;
   const fuzzRoot = findFuzzRoot();
   const root = findCargoProjectRoot();
-  const results = loadFunctionResults(path.join(projectPath, "output"));
-  if (!results && root) {
+  const outputPath = path.join(projectPath, "output");
+  if (!fs.existsSync(outputPath) && root) {
     vscode.window.withProgress(
       {
         location: vscode.ProgressLocation.Notification,
@@ -43,11 +43,11 @@ export async function useGlobalContext(context: vscode.ExtensionContext) {
         cancellable: false,
       },
       async () => {
-        // ⏳ Your long-running async task
         await runRustAnalyzer(context, root);
       }
     );
   }
+  const results = loadFunctionResults(path.join(projectPath, "output"));
   globalContext.fuzzRoot = fuzzRoot;
   globalContext.projectRoot = root;
   globalContext.results = results;
